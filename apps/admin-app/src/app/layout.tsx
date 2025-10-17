3f009b8fd61749e4ef08c84c2a58551c5d16b06a
Joel Vargas <vermudo.com@gmail.com>
2025-10-17 16:36:43 -0600

    feat(admin-app): wrap UserProvider with ReactQueryClientProvider for improved API query management

--- apps/admin-app/src/app/layout.tsx
+++ apps/admin-app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import UserProvider from "@/context/UserContext";
 import { Toaster } from "sonner";
+import { ReactQueryClientProvider } from "@/components/tw-blocks/providers/ReactQueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({ children }: { children: any }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <UserProvider>{children}</UserProvider>
+        <ReactQueryClientProvider>
+          <UserProvider>{children}</UserProvider>
 
-        <Toaster position="top-right" richColors />
+          <Toaster position="top-right" richColors />
+        </ReactQueryClientProvider>
       </body>
     </html>
   );
