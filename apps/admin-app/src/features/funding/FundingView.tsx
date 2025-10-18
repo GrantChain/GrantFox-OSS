dabb7e22a72d714d9eb00d005d6e48dc1ab3174c
Joel Vargas <vermudo.com@gmail.com>
2025-10-17 18:25:35 -0600

    feat(admin-app): enhance FundingView with wallet connection prompt

--- apps/admin-app/src/features/funding/FundingView.tsx
+++ apps/admin-app/src/features/funding/FundingView.tsx
@@ -6,9 +6,13 @@ import { EscrowCreatedSection } from "./sections/EscrowCreatedSection";
 import { LoadEscrow } from "./LoadEscrow";
 import { Fund } from "./Fund";
 import { Button } from "@/components/ui/button";
+import { useWalletContext } from "@/components/tw-blocks/wallet-kit/WalletProvider";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
+import { TriangleAlert } from "lucide-react";
 
 export const FundingView = () => {
   const { selectedEscrow, clearEscrow } = useEscrowContext();
+  const { walletAddress } = useWalletContext();
 
   return (
     <>
@@ -36,10 +40,25 @@ export const FundingView = () => {
       </div>
 
       <div className="flex flex-col md:flex-row gap-4 justify-between items-start">
-        <div className="flex flex-col gap-4 w-full md:w-2/5">
+        <div className="flex flex-col gap-4 w-full md:w-2/5 justify-center items-center">
           <LoadEscrow />
 
-          {selectedEscrow && <Fund />}
+          {selectedEscrow && walletAddress && <Fund />}
+          {selectedEscrow && !walletAddress && (
+            <Card className="w-full sm:w-1/2 gap-2">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  <TriangleAlert className="h-4 w-4 text-orange-500" /> Connect
+                  Wallet
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-muted-foreground">
+                  Please connect your wallet to fund the escrow.
+                </p>
+              </CardContent>
+            </Card>
+          )}
         </div>
 
         <div className="w-full md:w-3/5">
