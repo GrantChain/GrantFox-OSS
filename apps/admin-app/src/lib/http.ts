2c873bee5bcc9bc4499deba15064c26a3599a187
Joel Vargas <vermudo.com@gmail.com>
2025-10-20 11:22:52 -0600

    fix(http): update baseURL to use environment variable for API endpoint

--- apps/admin-app/src/lib/http.ts
+++ apps/admin-app/src/lib/http.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
