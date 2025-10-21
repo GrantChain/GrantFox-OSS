ab117c5b474c2e5ed6a604281c4d15eb00ad4a4d
Joel Vargas <vermudo.com@gmail.com>
2025-10-20 22:29:41 -0600

    feat(maintainer-app): add role validation. if you dont have any role, so it creates, otherwise, add the new role

---
+++ apps/maintainer-app/src/lib/api.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const httpUnauthorized = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 60000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const http = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 60000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+http.interceptors.request.use(
+  (config) => {
+    const projectRef = process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(
+      "https://",
+      ""
+    ).split(".")[0];
+    const key = `sb-${projectRef}-auth-token`;
+
+    const raw = localStorage.getItem(key);
+    if (raw) {
+      try {
+        const parsed = JSON.parse(raw);
+        config.headers["x-user-id"] = parsed?.user?.id;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export { http, httpUnauthorized };
