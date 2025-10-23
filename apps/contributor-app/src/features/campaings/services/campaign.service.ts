aa1e5e6edff85182c7a1466a2dc21cb8e37d1718
armandocodecr <totovip1464@gmail.com>
2025-10-22 23:37:51 -0600

    feat(contributor-app): add CampaignService and GithubUserService for campaign and user management

---
+++ apps/contributor-app/src/features/campaings/services/campaign.service.ts
@@ -0,0 +1,46 @@
+import { Campaign } from "@/types/campaign.type";
+import { ApiUser } from "@/types/user.type";
+import { AxiosInstance } from "axios";
+
+export class CampaignService {
+  private readonly http: AxiosInstance;
+
+  constructor(http: AxiosInstance) {
+    this.http = http;
+  }
+
+  async getActiveCampaign(): Promise<Campaign[]> {
+    try {
+      const { data } = await this.http.get("/campaigns?status=ACTIVE");
+      return data;
+    } catch (error) {
+      throw new Error("Failed to get active campaign", { cause: error });
+    }
+  }
+
+  async getContributorsCampaign(campaignId: string): Promise<ApiUser[]> {
+    try {
+      const { data } = await this.http.get(`campaign-contributors/campaign/${campaignId}`);
+      return data;
+    } catch (error) {
+      throw new Error("Failed to get active campaign", { cause: error });
+    }
+  }
+
+  async getAllCampaigns(): Promise<Campaign[]> {
+    try {
+      const { data } = await this.http.get("/campaigns");
+      return data;
+    } catch (error) {
+      throw new Error("Failed to get campaigns", { cause: error });
+    }
+  }
+
+  async registerContributor(campaignId: string, userId: string): Promise<void> {
+    try {
+      await this.http.post(`/campaign-contributors/campaign/${campaignId}/register`, { "x-user-id": userId });
+    } catch (error) {
+      throw new Error("Failed to register contributor", { cause: error });
+    }
+  }
+}
