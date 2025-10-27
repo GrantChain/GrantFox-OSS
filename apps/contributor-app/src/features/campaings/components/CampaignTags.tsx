691e8f96ec6e594b250a6976896f7aca9d7633a5
Joel Vargas <vermudo.com@gmail.com>
2025-10-27 11:42:17 -0600

    feat(campaigns): add Stellar branding with image support in CampaignTags component

--- apps/contributor-app/src/features/campaings/components/CampaignTags.tsx
+++ apps/contributor-app/src/features/campaings/components/CampaignTags.tsx
@@ -1,4 +1,6 @@
 import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import Link from "next/link";
 
 interface CampaignTagsProps {
   tags: string[] | undefined;
@@ -6,12 +8,35 @@ interface CampaignTagsProps {
 
 export default function CampaignTags({ tags }: CampaignTagsProps) {
   return (
-    <section className="w-3xl gap-4 flex justify-start items-center mt-5">
+    <section className="w-full gap-4 flex justify-between items-center mt-5">
       {tags?.map((tag) => (
         <Badge key={tag} variant="outline">
           {tag}
         </Badge>
       ))}
+
+      <Link href="https://stellar.org" target="_blank">
+        <div className="flex items-center gap-2">
+          <span className="text-sm text-muted-foreground font-bold">
+            Backed by
+          </span>
+
+          <Image
+            src={"/tech/stellar-dark.svg"}
+            className="block dark:hidden"
+            alt="GrantFox Stellar"
+            width={30}
+            height={30}
+          />
+          <Image
+            src={"/tech/stellar-light.svg"}
+            className="hidden dark:block"
+            alt="GrantFox Stellar Light"
+            width={30}
+            height={30}
+          />
+        </div>
+      </Link>
     </section>
   );
 }
