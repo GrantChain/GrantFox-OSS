b80642f21e282bff831a30ab579b558d030d91b9
Joel Vargas <vermudo.com@gmail.com>
2025-10-28 13:45:22 -0600

    feat(contributor-app): enhance formatDate function to optionally display hours and minutes

--- apps/contributor-app/src/lib/format.ts
+++ apps/contributor-app/src/lib/format.ts
@@ -1,7 +1,13 @@
-export function formatDate(dateString: string) {
+export function formatDate(
+  dateString: string,
+  showHours: boolean = false,
+  showMinutes: boolean = false
+) {
   return new Date(dateString).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
+    hour: showHours ? "2-digit" : undefined,
+    minute: showMinutes ? "2-digit" : undefined,
   });
 }
