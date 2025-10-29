ff301f5ea72ccff255a663cef1b0f2f04b5c1a11
Joel Vargas <vermudo.com@gmail.com>
2025-10-28 19:45:41 -0600

    style(campaigns): update layout of CampaignRepositoriesList to use grid for better responsiveness

--- apps/contributor-app/src/features/campaings/components/CampaignRepositoriesList.tsx
+++ apps/contributor-app/src/features/campaings/components/CampaignRepositoriesList.tsx
@@ -13,7 +13,7 @@ export const CampaignRepositoriesList = ({
   org = undefined,
 }: CampaignRepositoriesListProps) => {
   return (
-    <div className="w-full flex flex-col gap-8">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {repositories.length > 0 ? (
         repositories.map((repo) => (
           <RepositoryCard key={repo.github_repo_id} repo={repo} org={org} />
