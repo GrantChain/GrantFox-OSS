4df434dcc749b450d7eeaf4a81040621a473a426
Joel Vargas <vermudo.com@gmail.com>
2025-11-06 13:08:09 -0600

    feat(maintainer-app): update project links to open in new tab and add external link icon

--- apps/maintainer-app/src/features/campaings/components/CampaignProjectsList.tsx
+++ apps/maintainer-app/src/features/campaings/components/CampaignProjectsList.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 import type { Project } from "@/types/project.type";
 import { ShineBorder } from "@/components/ui/shine-border";
+import { ExternalLink } from "lucide-react";
 
 interface CampaignProjectsListProps {
   projects: Project[];
@@ -18,26 +19,32 @@ export const CampaignProjectsList = ({
       {projects.length > 0 ? (
         projects.map((proj) => (
           <Link
-            href={`/campaigns/org/${proj.github_handle}`}
+            href={`https://contribute.grantfox.xyz/campaigns/org/${proj.github_handle}`}
+            target="_blank"
+            rel="noopener noreferrer"
             className="text-lg font-medium"
             key={proj.project_id}
           >
-            <div className="relative rounded-2xl border p-5 bg-gradient-to-b from-background/40 to-background/10 hover:from-accent/10 hover:to-transparent transition-all shadow-sm hover:shadow-md hover:-translate-y-0.5 group overflow-hidden">
+            <div className="relative rounded-2xl border p-5 bg-gradient-to-b from-background/40 to-background/10 hover:from-accent/10 hover:to-transparent transition-all shadow-sm hover:shadow-md hover:-translate-y-0.5 group overflow-hidden min-h-30">
               <ShineBorder
                 className="pointer-events-none"
                 shineColor={["#7c3aed33", "#22d3ee33"]}
               />
               <div className="flex items-center justify-between">
-                <div className="flex items-center gap-3">
-                  <Image
-                    src={`https://github.com/${proj.github_handle}.png`}
-                    alt={proj.name}
-                    width={32}
-                    height={32}
-                    className="rounded-full"
-                  />
+                <div className="flex justify-between w-full items-center gap-3">
+                  <div className="flex items-center gap-3">
+                    <Image
+                      src={`https://github.com/${proj.github_handle}.png`}
+                      alt={proj.name}
+                      width={32}
+                      height={32}
+                      className="rounded-full"
+                    />
 
-                  {proj.name}
+                    {proj.name}
+                  </div>
+
+                  <ExternalLink className="hidden group-hover:block size-4 text-muted-foreground" />
                 </div>
               </div>
               <p className="mt-2 text-sm text-muted-foreground line-clamp-2">
