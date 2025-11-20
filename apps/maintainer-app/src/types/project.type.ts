a9c317fa2451c913af0874d8047999fedd1b2639
JoelVR17 <vermudo.com@gmail.com>
2025-11-20 06:11:50 -0600

    feat(maintainer-app): update Project interface to use Maintainer and Repository types for improved type safety

--- apps/maintainer-app/src/types/project.type.ts
+++ apps/maintainer-app/src/types/project.type.ts
@@ -1,3 +1,6 @@
+import { Maintainer } from "./maintainer.type";
+import { Repository } from "./repositories.type";
+
 export interface Project {
   project_id: string;
   name: string;
@@ -11,8 +14,11 @@ export interface Project {
   created_at: string;
   updated_at: string;
   reviewed_at: string;
-  maintainers: string[]; // todo: add maintainer type
-  repositories: string[]; // todo: add repository type
+  maintainers: Maintainer[];
+  repositories: Pick<
+    Repository,
+    "github_repo_id" | "name" | "github_url" | "description"
+  >[];
 }
 
 export interface ValidateGitHubHandleResponse {
